8941ef4ec6a0f25935c9e7e9cf1072d9c415a397
cynthia clinton <cynthiaclinton@cynthias-MBP.fios-router.home>
2015-11-25 16:26:08 -0500

    Putting finishing touches on numerology app

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,70 @@
+#Show people landing page
+get '/people' do
+  @people = Person.all
+  erb :"/people/index"
+end
+
+#Shows a new person page
+get '/people/new' do
+  @person = Person.new
+  erb :"/people/new"
+end
+
+#Sumit form by creating a new Person object, checks to see if valid data entered and redirect to show page when finished
+post '/people' do
+  if params[:birthdate].include?("-")
+    birthdate = params[:birthdate]
+  else
+    birthdate = Date.strptime(params[:birthdate], "%m%d%Y")
+  end
+  
+  @person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
+  
+  if @person.valid?
+    @person.save
+    redirect "/people/#{@person.id}"
+  else
+    @person.errors.full_messages.each do |msg|
+      @errors = "#{@errors} #{msg}."
+    end
+    erb :"/people/new"
+  end
+end
+
+#Shows eidt a person page
+get '/people/:id/edit' do
+  @person = Person.find(params[:id])
+  erb :"/people/edit"
+end
+
+#Edits individual person and checks to see if valid data is entered and redirect to show the edit
+put '/people/:id' do
+  @person = Person.find(params[:id])
+  @person.first_name = params[:first_name]
+  @person.last_name = params[:last_name]
+  @person.birthdate = params[:birthdate]
+  if @person.valid?
+    @person.save
+    redirect "/people/#{@person.id}"
+  else
+    @person.errors.full_messages.each do |msg|
+      @errors = "#{@errors} #{msg}."
+    end
+    erb :"/people/edit"    
+  end
+end
+
+# Delete individual person
+delete '/people/:id' do
+  person = Person.find(params[:id])
+  person.delete
+  redirect "/people"
+end
+
+# Show individual person page
+get '/people/:id' do
+  @person = Person.find(params[:id])
+  birth_path_num = Person.get_birth_path_num(@person.birthdate.strftime("%m%d%Y"))
+  @message = Person.get_message(birth_path_num)  
+  erb :"/people/show"
+end
